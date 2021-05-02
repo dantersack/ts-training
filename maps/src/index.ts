95c61ce9446cf92173a1fae06d9e856c72406d07
dante <sack_dante@hotmail.com>
2021-05-02 15:28:33 -0300

    add custom Map class

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,5 +1,6 @@
-import {User} from './User'
-import {Company} from './Company'
+import { Map } from './Map'
+import { User } from './User'
+import { Company } from './Company'
 
 const user = new User()
 const company = new Company()
@@ -7,10 +8,4 @@ const company = new Company()
 console.log(user)
 console.log(company)
 
-new google.maps.Map(document.getElementById('map'), {
-    zoom: 1,
-    center: {
-        lat: 0,
-        lng: 0,
-    }
-})
+const map = new Map('map')
