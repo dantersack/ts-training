d8bac70b102afa9395062dfbeb6385dfc12dd6c7
dante <sack_dante@hotmail.com>
2021-05-01 21:49:18 -0300

    type annotations with functions and objects

---
+++ annotations/objects.ts
@@ -0,0 +1,18 @@
+const profile = {
+    name: 'dante',
+    age: 28,
+    coords: {
+        lat: 0,
+        lng: 15
+    },
+    setAge(age: number): void {
+        this.age = age
+    }
+}
+
+const {age}: {age: number} = profile
+const {coords: {lat, lng}}: {coords: {lat: number, lng: number}} = profile
+
+console.log(age)
+console.log(lat)
+console.log(lng)
