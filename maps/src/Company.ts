07ae136b58978f7f77540afa067f756d2fd9f2a0
dante <sack_dante@hotmail.com>
2021-05-02 18:50:02 -0300

    add popup windows

--- maps/src/Company.ts
+++ maps/src/Company.ts
@@ -1,6 +1,8 @@
 import faker from 'faker'
 
-export class Company {
+import { Mappable } from './Map'
+
+export class Company implements Mappable {
     name: string
     catchPhrase: string
     location: {
@@ -15,5 +17,9 @@ export class Company {
             lat: parseFloat(faker.address.latitude()),
             lng: parseFloat(faker.address.longitude()),
         }
+    }
+
+    markerContent(): string {
+        return `Company ${this.name} here`
     }
 }
