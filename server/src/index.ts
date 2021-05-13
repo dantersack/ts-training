1bc0f927b73096d725cf5d688998e2421092ac37
dante <sack_dante@hotmail.com>
2021-05-13 12:08:57 -0300

    remove build folder

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,11 @@
-import express, { Request, Response } from 'express'
+import express from 'express'
+
+import { router } from './routes/loginRoutes'
 
 const app = express()
 
-app.get('/', (req: Request, res: Response) => {
-    res.send(`
-        <div>
-            <h1>hello world</h1>
-        </div>
-    `)
-})
+app.use(express.urlencoded())
+app.use(router)
 
 app.listen(3000, () => {
     console.log('Listening on port 3000')
