d98d470a53cded35afee8144a332b77b58eea3ac
dante <sack_dante@hotmail.com>
2021-05-13 14:34:54 -0300

    add login, logout and protected routes

--- server/src/routes/loginRoutes.ts
+++ server/src/routes/loginRoutes.ts
@@ -1,13 +1,37 @@
-import { Request, Response, Router } from 'express'
+import { NextFunction, Request, Response, Router } from 'express'
 
 interface RequestWithBody extends Request {
     body: {[key: string]: string | undefined}
 }
 
+function requireAuth(req: Request, res: Response, next: NextFunction): void {
+    if (req.session && req.session.logged) {
+        next()
+        return
+    }
+
+    res.status(403)
+    res.send('You shall not pass')
+}
+
 const router = Router()
 
 router.get('/', (req: Request, res: Response) => {
-    res.send('hi there')
+    if (req.session && req.session.logged) {
+        res.send(`
+            <div>
+                <div>You are logged in</div>
+                <a href="/logout">Logout</a>
+            </div>
+        `)
+    } else {
+        res.send(`
+            <div>
+                <div>You are not logged in</div>
+                <a href="/login">Login</a>
+            </div>
+        `)
+    }
 })
 
 router.get('/login', (req: Request, res: Response) => {
@@ -35,6 +59,15 @@ router.post('/login', (req: RequestWithBody, res: Response) => {
     } else {
         res.send('Invalid email or password')
     }
+})
+
+router.get('/logout', (req: Request, res: Response) => {
+    req.session = undefined
+    res.redirect('/')
+})
+
+router.get('/protected', requireAuth, (req: Request, res: Response) => {
+    res.send('Welcome to protected routes')
 })
 
 export {router}
