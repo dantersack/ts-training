0c6054bb771ec36eeaeffa80f21d3d7ac23ee176
dante <sack_dante@hotmail.com>
2021-05-13 12:26:02 -0300

    wiring up sessions

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express'
+import cookieSession from 'cookie-session'
 
 import { router } from './routes/loginRoutes'
 
 const app = express()
 
 app.use(express.urlencoded())
+app.use(cookieSession({keys: ['qwerty']}))
 app.use(router)
 
 app.listen(3000, () => {
